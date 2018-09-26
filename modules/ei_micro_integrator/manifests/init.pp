bbd389732c18e949a00bd3fd69ce4d6c0c59e3a0
irunika <uvoofLakmal075>
2018-09-26 15:21:04 +0530

    Add micro-integrator profile

---
+++ modules/ei_micro_integrator/manifests/init.pp
@@ -0,0 +1,171 @@
+# ----------------------------------------------------------------------------
+#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ----------------------------------------------------------------------------
+
+# Class: ei_micro_integrator
+# Init class of EI Integrator default profile
+class ei_micro_integrator (
+  $user = $ei_micro_integrator::params::user,
+  $user_id = $ei_micro_integrator::params::user_id,
+  $user_group = $ei_micro_integrator::params::user_group,
+  $user_group_id = $ei_micro_integrator::params::user_group_id,
+  $product = $ei_micro_integrator::params::product,
+  $product_version = $ei_micro_integrator::params::product_version,
+  $profile = $ei_micro_integrator::params::profile,
+  $service_name = $ei_micro_integrator::params::service_name,
+  $template_list = $ei_micro_integrator::params::template_list,
+  $jre_version = $ei_micro_integrator::params::jre_version,
+  $start_script_template = $ei_micro_integrator::params::start_script_template,
+
+  # ------ Configuration Params ------ #
+
+  # master-datasources.xml
+  $carbon_db_url = $ei_micro_integrator::params::carbon_db_url,
+  $carbon_db_username = $ei_micro_integrator::params::carbon_db_username,
+  $carbon_db_password = $ei_micro_integrator::params::carbon_db_password,
+  $carbon_db_driver = $ei_micro_integrator::params::carbon_db_driver,
+
+  # carbon.xml
+  $security_keystore_location = $ei_micro_integrator::params::security_keystore_location,
+  $security_keystore_type = $ei_micro_integrator::params::security_keystore_type,
+  $security_keystore_password = $ei_micro_integrator::params::security_keystore_password,
+  $security_keystore_key_alias = $ei_micro_integrator::params::security_keystore_key_alias,
+  $security_keystore_key_password = $ei_micro_integrator::params::security_keystore_key_password,
+
+  $security_trust_store_location = $ei_micro_integrator::params::security_trust_store_location,
+  $security_trust_store_type = $ei_micro_integrator::params::security_trust_store_type,
+  $security_trust_store_password = $ei_micro_integrator::params::security_trust_store_password,
+
+  # axis2.xml
+  $transport_receiver_keystore_location = $ei_micro_integrator::params::transport_receiver_keystore_location,
+  $transport_receiver_keystore_type = $ei_micro_integrator::params::transport_receiver_keystore_type,
+  $transport_receiver_keystore_password = $ei_micro_integrator::params::transport_receiver_keystore_password,
+  $transport_receiver_keystore_key_password = $ei_micro_integrator::params::transport_receiver_keystore_key_password,
+
+  $transport_receiver_trust_store_location = $ei_micro_integrator::params::transport_receiver_trust_store_location,
+  $transport_receiver_trust_store_type = $ei_micro_integrator::params::transport_receiver_trust_store_type,
+  $transport_receiver_trust_store_password = $ei_micro_integrator::params::transport_receiver_trust_store_password,
+
+  $transport_sender_keystore_location = $ei_micro_integrator::params::transport_sender_keystore_location,
+  $transport_sender_keystore_type = $ei_micro_integrator::params::transport_sender_keystore_type,
+  $transport_sender_keystore_password = $ei_micro_integrator::params::transport_sender_keystore_password,
+  $transport_sender_keystore_key_password = $ei_micro_integrator::params::transport_sender_keystore_key_password,
+
+  $transport_sender_trust_store_location = $ei_micro_integrator::params::transport_sender_trust_store_location,
+  $transport_sender_trust_store_type = $ei_micro_integrator::params::transport_sender_trust_store_type,
+  $transport_sender_trust_store_password = $ei_micro_integrator::params::transport_sender_trust_store_password,
+)
+
+  inherits ei_micro_integrator::params {
+
+  if $::osfamily == 'redhat' {
+    $ei_package = 'wso2ei-linux-installer-x64-6.3.0.rpm'
+    $installer_provider = 'rpm'
+    $install_path = "/usr/lib64/wso2/${product}/${product_version}"
+  }
+  elsif $::osfamily == 'debian' {
+    $ei_package = 'wso2ei-linux-installer-x64-6.3.0.deb'
+    $installer_provider = 'dpkg'
+    $install_path = "/usr/lib/wso2/${product}/${product_version}"
+  }
+
+  # Create wso2 group
+  group { $user_group:
+    ensure => present,
+    gid    => $user_group_id,
+    system => true,
+  }
+
+  # Create wso2 user
+  user { $user:
+    ensure => present,
+    uid    => $user_id,
+    gid    => $user_group_id,
+    home   => "/home/${user}",
+    system => true,
+  }
+
+  # Ensure the installation directory is available
+  file { "/opt/${product}":
+    ensure => 'directory',
+    owner  => $user,
+    group  => $user_group,
+  }
+
+  # Copy the installer to the directory
+  file { "/opt/${product}/${ei_package}":
+    owner  => $user,
+    group  => $user_group,
+    mode   => '0644',
+    source => "puppet:///modules/${module_name}/${ei_package}",
+  }
+
+  # Install WSO2 API Manager
+  package { $product:
+    ensure   => installed,
+    provider => $installer_provider,
+    source   => "/opt/${product}/${ei_package}"
+  }
+
+  # Change the ownership of the installation directory to wso2 user & group
+  file { $install_path:
+    ensure  => directory,
+    owner   => $user,
+    group   => $user_group,
+    require => [ User[$user], Group[$user_group]],
+    recurse => true
+  }
+
+  # Copy configuration changes to the installed directory
+  $template_list.each | String $template | {
+    file { "${install_path}/${template}":
+      ensure  => file,
+      owner   => $user,
+      group   => $user_group,
+      mode    => '0644',
+      content => template("${module_name}/carbon-home/${template}.erb")
+    }
+  }
+
+  # Copy wso2server.sh to installed directory
+  file { "${install_path}/${start_script_template}":
+    ensure  => file,
+    owner   => $user,
+    group   => $user_group,
+    mode    => '0754',
+    content => template("${module_name}/carbon-home/${start_script_template}.erb")
+  }
+
+  # Copy the unit file required to deploy the server as a service
+  file { "/etc/systemd/system/${service_name}.service":
+    ensure  => present,
+    owner   => root,
+    group   => root,
+    mode    => '0754',
+    content => template("${module_name}/${service_name}.service.erb"),
+  }
+
+  /*
+    Following script can be used to copy file to a given location.
+    This will copy some_file to install_path -> repository.
+    Note: Ensure that file is available in modules -> apim -> files
+  */
+  # file { "${install_path}/repository/some_file":
+  #   owner  => $user,
+  #   group  => $user_group,
+  #   mode   => '0644',
+  #   source => "puppet:///modules/${module_name}/some_file",
+  # }
+}
---
+++ modules/ei_micro_integrator/manifests/params.pp
@@ -0,0 +1,79 @@
+# ----------------------------------------------------------------------------
+#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+# ----------------------------------------------------------------------------
+
+# Claas ei_micro_integrator::params
+# This class includes all the necessary parameters.
+class ei_micro_integrator::params {
+  $user = 'wso2carbon'
+  $user_id = 802
+  $user_group = 'wso2'
+  $user_home = '/home/$user'
+  $user_group_id = 802
+  $product = 'wso2ei'
+  $product_version = '6.3.0'
+  $profile = 'micro-integrator'
+  $hostname = 'localhost'
+  $service_name = "${product}-${profile}"
+  $mgt_hostname = 'localhost'
+  $jre_version = 'jre1.8.0_172'
+
+  # Define the template
+  $start_script_template = "wso2/micro-integrator/bin/wso2server.sh"
+  $template_list = [
+    'wso2/micro-integrator/conf/datasources/master-datasources.xml',
+    'wso2/micro-integrator/conf/carbon.xml',
+    'wso2/micro-integrator/conf/axis2/axis2.xml',
+    # 'wso2/micro-integrator/conf/tomcat/catalina-server.xml',
+  ]
+
+  # ------ Configuration Params ------ #
+
+  # master-datasources.xml
+  $carbon_db_url = 'jdbc:h2:./repository/database/WSO2CARBON_DB;DB_CLOSE_ON_EXIT=FALSE;LOCK_TIMEOUT=60000'
+  $carbon_db_username = 'wso2carbon'
+  $carbon_db_password = 'wso2carbon'
+  $carbon_db_driver = 'org.h2.Driver'
+
+  # carbon.xml
+  $security_keystore_location = '${carbon.home}/repository/resources/security/wso2carbon.jks'
+  $security_keystore_type = 'JKS'
+  $security_keystore_password = 'wso2carbon'
+  $security_keystore_key_alias = 'wso2carbon'
+  $security_keystore_key_password = 'wso2carbon'
+
+  $security_trust_store_location = '${carbon.home}/repository/resources/security/client-truststore.jks'
+  $security_trust_store_type = 'JKS'
+  $security_trust_store_password = 'wso2carbon'
+
+  # axis2.xml
+  $transport_receiver_keystore_location = 'repository/resources/security/wso2carbon.jks'
+  $transport_receiver_keystore_type = 'JKS'
+  $transport_receiver_keystore_password = 'wso2carbon'
+  $transport_receiver_keystore_key_password = 'wso2carbon'
+
+  $transport_receiver_trust_store_location = 'repository/resources/security/client-truststore.jks'
+  $transport_receiver_trust_store_type = 'JKS'
+  $transport_receiver_trust_store_password = 'wso2carbon'
+
+  $transport_sender_keystore_location = 'repository/resources/security/wso2carbon.jks'
+  $transport_sender_keystore_type = 'JKS'
+  $transport_sender_keystore_password = 'wso2carbon'
+  $transport_sender_keystore_key_password = 'wso2carbon'
+
+  $transport_sender_trust_store_location = 'repository/resources/security/client-truststore.jks'
+  $transport_sender_trust_store_type = 'JKS'
+  $transport_sender_trust_store_password = 'wso2carbon'
+}
