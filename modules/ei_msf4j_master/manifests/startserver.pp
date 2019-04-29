65dae49f04b11a25017c3afbf9d03789e466ca28
Savidude <mysticsavy@gmail.com>
2019-04-29 09:33:33 +0530

    Update msf4j resources for continuous update delivery

--- modules/ei_msf4j_master/manifests/startserver.pp
+++ modules/ei_msf4j_master/manifests/startserver.pp
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#  Copyright (c) 2019 WSO2, Inc. http://www.wso2.org
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -16,17 +16,7 @@
 
 # Class ei_msf4j::startserver
 # Starts the server as a service in the final stage.
-class ei_msf4j::startserver inherits ei_msf4j::params {
+class ei_msf4j_master::startserver inherits ei_msf4j_master::params {
 
-  exec { 'daemon-reload':
-    command => "systemctl daemon-reload",
-    path    => "/bin/",
-  }
-
-  # Start the service
-  service { $service_name:
-    enable => true,
-    ensure => running,
-    subscribe => File["binary"],
-  }
+  # This class should be used to control Identity Server if required.
 }
