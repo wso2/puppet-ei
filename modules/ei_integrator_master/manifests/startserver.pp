d5f2181484db0d1317f710d33c01a4531eb65b79
Savidude <mysticsavy@gmail.com>
2019-04-29 09:10:51 +0530

    Update integrator resources for continuous update delivery

--- modules/ei_integrator_master/manifests/startserver.pp
+++ modules/ei_integrator_master/manifests/startserver.pp
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------------
-#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#  Copyright (c) 2019 WSO2, Inc. http://www.wso2.org
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -14,19 +14,9 @@
 #  limitations under the License.
 # ----------------------------------------------------------------------------
 
-# Class ei_integrator::startserver
+# Class ei_integrator_master::startserver
 # Starts the server as a service in the final stage.
-class ei_integrator::startserver inherits ei_integrator::params {
+class ei_integrator_master::startserver inherits ei_integrator_master::params {
 
-  exec { 'daemon-reload':
-    command => "systemctl daemon-reload",
-    path    => "/bin/",
-  }
-
-  # Start the service
-  service { $service_name:
-    enable => true,
-    ensure => running,
-    subscribe => File["binary"],
-  }
+  # This class should be used to control Identity Server if required.
 }
