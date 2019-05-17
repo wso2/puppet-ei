3d0805bc04db67b875f47bbda1140face82b927b
Savidude <mysticsavy@gmail.com>
2019-05-17 13:20:02 +0530

    Run master modules with puppet apply

--- modules/ei_msf4j_master/build.sh
+++ modules/ei_msf4j_master/build.sh
@@ -30,8 +30,7 @@
 : ${puppet_env:="/etc/puppet/code/environments/production"}
 
 # Apply configurations
-export FACTER_profile=ei_msf4j_master
-puppet agent -vt
+puppet apply -e "include ei_msf4j_master" --modulepath=${puppet_env}/modules
 
 # exit immediately if a command exits with a non-zero status
 set -e
