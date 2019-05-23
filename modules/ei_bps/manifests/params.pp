4de54d10471a49424ac31463c31d7ad10a3d9c65
Savidude <mysticsavy@gmail.com>
2019-05-23 12:04:31 +0530

    Migration of WSO2 Enterprise Integrator 6.5.0

--- modules/ei_bps/manifests/params.pp
+++ modules/ei_bps/manifests/params.pp
@@ -20,7 +20,7 @@ class ei_bps::params {
   $user = 'wso2carbon'
   $user_group = 'wso2'
   $product = 'wso2ei'
-  $product_version = '6.4.0'
+  $product_version = '6.5.0'
   $profile = 'business-process'
   $service_name = "${product}-${profile}"
   # $local_ip = $::ipaddress
