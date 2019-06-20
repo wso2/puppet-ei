a21044544e399ae50c82a1b08f025181af4e2a0c
Savidude <mysticsavy@gmail.com>
2019-06-20 10:54:42 +0530

    Remove duplicate code

--- modules/ei_common/manifests/service.pp
+++ modules/ei_common/manifests/service.pp
@@ -1,5 +1,5 @@
 #----------------------------------------------------------------------------
-#  Copyright (c) 2018 WSO2, Inc. http://www.wso2.org
+#  Copyright (c) 2019 WSO2, Inc. http://www.wso2.org
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -9,20 +9,15 @@
 #
 #  Unless required by applicable law or agreed to in writing, software
 #  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either excustomss or implied.
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 #----------------------------------------------------------------------------
 
-# Run stages
-stage { 'custom': }
+class ei_common::service inherits ei_common::params {
 
-# Order stages
-Stage['main'] -> Stage['custom']
-
-node default {
-  class { "::${::profile}": }
-  class { "::${::profile}::custom":
-    stage => 'custom'
+  service { "${wso2_service_name}":
+    enable => true,
+    ensure => running,
   }
 }
